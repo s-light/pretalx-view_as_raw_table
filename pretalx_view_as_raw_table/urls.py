3199e67be513128b5a52829fdb71cfb6c240b608
s-light <github@s-light.eu>
2019-02-20 11:34:52 +0100

    extended Hello World works

--- pretalx_view_as_raw_table/urls.py
+++ pretalx_view_as_raw_table/urls.py
@@ -4,19 +4,13 @@ from django.conf.urls import url
 
 from pretalx.event.models.event import SLUG_CHARS
 
-# from .views import helloworld
 from .views import rawtable
 
 urlpatterns = [
-    # url(
-    #     f'^orga/event/(?P<event>[{SLUG_CHARS}]+)/helloworld',
-    #     helloworld.world,
-    #     name='helloworld'
-    # ),
     url(
         f'^orga/event/(?P<event>[{SLUG_CHARS}]+)/raw_table',
         # rawtable.RawTable,
-        rawtable.world,
+        rawtable.RawTable.as_view(),
         name='rawtable'
     ),
 ]
--- pretalx_view_as_raw_table/views/rawtable.py
+++ pretalx_view_as_raw_table/views/rawtable.py
@@ -1,15 +1,15 @@
 """."""
 
-import json
-
 from django.urls import resolve, reverse
 from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
+from django.views.generic import ListView, TemplateView, View
 from pretalx.orga.signals import nav_event
 
 from pretalx.common.mixins.views import EventPermissionRequired
 
 from pretalx.submission.models import Submission
+
 
 @receiver(nav_event, dispatch_uid='pretalx_view_as_raw_table__submissions')
 def navbar_info(sender, request, **kwargs):
@@ -30,46 +30,46 @@ def navbar_info(sender, request, **kwargs):
     }]
 
 
-class RawTable(EventPermissionRequired):
+class RawTable(EventPermissionRequired, ListView):
     """Raw Table View."""
 
-    # model = Submission
+    model = Submission
     context_object_name = 'test'
     # template_name = 'pretalx_view_as_raw_table/submission/table.html'
     template_name = 'pretalx_view_as_raw_table/test.html'
     permission_required = 'orga.view_submissions'
 
-    def dispatch(self, *args, **kwargs):
-        if self.request.user.has_perm('orga.view_speakers', self.request.event):
-            self.default_filters.add('speakers__name__icontains')
-        return super().dispatch(*args, **kwargs)
-    #
-    # def get_queryset(self):
-    #     qs = (
-    #         self.request.event.submissions(manager='all_objects')
-    #         .select_related('submission_type')
-    #         .order_by('-id')
-    #         .all()
-    #     )
-    #     return qs.distinct()
-    #
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-    #     dates = data.keys()
-    #     if len(dates) > 1:
-    #         date_range = rrule.rrule(
-    #             rrule.DAILY,
-    #             count=(max(dates) - min(dates)).days + 1,
-    #             dtstart=min(dates),
-    #         )
-    #         if len(data) > 1:
-    #             context['timeline_data'] = json.dumps(
-    #                 [
-    #                     {"x": date.isoformat(), "y": data.get(date.date(), 0)}
-    #                     for date in date_range
-    #                 ]
-    #             )
-    #     return context
+    # def dispatch(self, *args, **kwargs):
+    #     if self.request.user.has_perm('orga.view_speakers', self.request.event):
+    #         self.default_filters.add('speakers__name__icontains')
+    #     return super().dispatch(*args, **kwargs)
+    # #
+    # # def get_queryset(self):
+    # #     qs = (
+    # #         self.request.event.submissions(manager='all_objects')
+    # #         .select_related('submission_type')
+    # #         .order_by('-id')
+    # #         .all()
+    # #     )
+    # #     return qs.distinct()
+    # #
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    # #     dates = data.keys()
+    # #     if len(dates) > 1:
+    # #         date_range = rrule.rrule(
+    # #             rrule.DAILY,
+    # #             count=(max(dates) - min(dates)).days + 1,
+    # #             dtstart=min(dates),
+    # #         )
+    # #         if len(data) > 1:
+    # #             context['timeline_data'] = json.dumps(
+    # #                 [
+    # #                     {"x": date.isoformat(), "y": data.get(date.date(), 0)}
+    # #                     for date in date_range
+    # #                 ]
+    # #             )
+    # #     return context
 
 
 def world(request, event):
